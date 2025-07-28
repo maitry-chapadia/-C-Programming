9fa02b2f8e2d1b0ef797b06bbf520a2e40b1e7df
maitry-chapadia <chapadiamaitry@gmail.com>
2025-07-28 15:50:51 +0530

    Add files via upload

---
+++ practical 6/temperature.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float c;
+printf("Enter the values of temperature on celcius = ");
+scanf("%f",&c);
+float f=((9/5)*c)+32;
+printf("the value in fahrenheit is %f",f);
+printf("this is done by maitry chapadia  25TCECQL");
+return 0;
+}
