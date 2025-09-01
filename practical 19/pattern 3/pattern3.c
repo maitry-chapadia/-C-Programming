b806dce26117f472c42aa31cc6ab4070c2dca870
maitry-chapadia <chapadiamaitry@gmail.com>
2025-09-01 15:55:59 +0530

    Add files via upload

---
+++ practical 19/pattern 3/pattern3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+
+    for (int i = n; i >= 1; i--) {
+        for (int s = 0; s < n - i; s++) printf("  ");
+
+        for (int k = i; k >= 1; k--) printf("%d ", k);
+        for (int k = 2; k <= i; k++) printf("%d ", k);
+
+        printf("\n");
+    }
+
+    for (int i = 2; i <= n; i++) {
+        for (int s = 0; s < n - i; s++) printf("  ");
+
+        for (int k = i; k >= 1; k--) printf("%d ", k);
+        for (int k = 2; k <= i; k++) printf("%d ", k);
+
+        printf("\n");
+    }
+
+    return 0;
+}
